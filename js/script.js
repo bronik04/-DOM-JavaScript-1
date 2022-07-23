571f3cf58d80db8201ea4df4d8cc0d8bb6e3906b
Nikolay Brovkin <bronik004@gmail.com>
2022-07-23 18:52:34 +0700

    finish

--- js/script.js
+++ js/script.js
@@ -1,59 +1,85 @@
-/* JavaScript код */
 'use strict';
 
-const buttonThemeLight = document.querySelector(".theme-button-light");
-const buttonThemeDark = document.querySelector(".theme-button-dark");
-const buttonThemeTexture = document.querySelector(".theme-button-texture");
-const buttonCardViewTile = document.querySelector(".card-view-button-tile");
-const buttonCardViewSrandard = document.querySelector(".card-view-button-standard");
-const buttonCardViewCompact = document.querySelector(".card-view-button-compact");
-const courses = document.querySelector(".cards");
-
-const buttonCardViewTileHandler = () => {
-  courses.classList.remove("standard");
-  courses.classList.remove("compact");
-  courses.classList.add("tile");
+const SettingType = {
+  ATTRIBUTE: 'attribute',
+  CLASS: 'class',
 };
 
-const buttonCardViewStandardHandler = () => {
-  courses.classList.remove("tile");
-  courses.classList.remove("compact");
-  courses.classList.add("standard");
+const containers = document.querySelectorAll('.js-buttons-container');
+
+const settingButtons = document.querySelectorAll('[data-setting-name]');
+
+const setDataAttribute = ({ settingTarget }, params) => {
+  const element = document.querySelector(settingTarget);
+  for (const [key, value] of Object.entries(params)) {
+    element.dataset[key] = value;
+  }
 };
 
-const buttonCardViewCompactHandler = () => {
-  courses.classList.remove("standard");
-  courses.classList.remove("tile");
-  courses.classList.add("compact");
+const setClass = ({ settingTarget }, params) => {
+  const element = document.querySelector(settingTarget);
+
+  for (const [key, value] of Object.entries(params)) {
+    const elements = Array.from(settingButtons)
+      .filter((element) => element.dataset['settingName'] === key);
+    // удалить классы, название которых совпадает со значением атрибута setting-value элементов с setting-name равным key
+    elements.forEach((item) => element.classList.remove(item.dataset.settingValue));
+
+    element.classList.add(value);
+  }
 };
 
-const buttonThemeLightHandler = () => {
-  document.documentElement.dataset['themeName'] = 'light';
+const setButtonActive = (params) => {
+  for (const [key, value] of Object.entries(params)) {
+    // находим активную кнопку для настройки
+    const activeButton = Array.from(settingButtons)
+      .find((element) => element.dataset['settingName'] === key && element.classList.contains('active'));
+
+    // находим кнопку, которую устанавливаем для настройки
+    const setButton = Array.from(settingButtons)
+      .find((element) => element.dataset['settingName'] === key && element.dataset['settingValue'] === value);
+
+    // снимаем класс active с кнопки, которая ранее была активной
+    activeButton.classList.remove('active');
+
+    // добавляем класс active с кнопки, которую устанавливаем
+    setButton.classList.add('active');
+  }
 };
 
-const buttonThemeDarkHandler = () => {
-  document.documentElement.dataset['themeName'] = 'dark';
+const applySetting = (setting, params) => {
+  if (setting.settingType === SettingType.CLASS) {
+    setClass(setting, params);
+  } else if (setting.settingType === SettingType.ATTRIBUTE) {
+    setDataAttribute(setting, params);
+  }
+
+  setButtonActive(params);
 };
 
-const buttonThemeTextureHandler = () => {
-  document.documentElement.dataset['themeName'] = 'texture';
+const settingButtonClickHandler = (evt, setting) => {
+  const button = evt.target.closest('button');
+
+  if (!button) {
+    return;
+  }
+
+  const params = {};
+
+  const { settingName, settingValue } = button.dataset;
+  params[settingName] = settingValue;
+
+  applySetting(setting, params);
 };
 
 const init = () => {
-  buttonCardViewTile.addEventListener("click", buttonCardViewTileHandler);
-  buttonCardViewSrandard.addEventListener("click", buttonCardViewStandardHandler);
-  buttonCardViewCompact.addEventListener("click", buttonCardViewCompactHandler);
+  containers.forEach((container) => {
+    const setting = container.dataset;
 
-  buttonThemeLight.addEventListener("click", buttonThemeLightHandler);
-  buttonThemeDark.addEventListener("click", buttonThemeDarkHandler);
-  buttonThemeTexture.addEventListener("click", buttonThemeTextureHandler);
+    container.addEventListener('click', (evt) => {
+      settingButtonClickHandler(evt, setting);
+    });
+  });
 };
 
 init();
-
-
-
-
-
-
-
